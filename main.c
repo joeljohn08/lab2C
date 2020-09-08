e944ddcb365c89a7652f6d4a376c1a12a74089ca
JoelJohn08 <jmj6182@psu.edu>
2020-09-08 14:21:30 +0000

    B-

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(void)
    }
    else if (grade >= 80.0)
    { 
-     printf("Your letter grade for CMPSC 131 is B.\n");
+     printf("Your letter grade for CMPSC 131 is B-.\n");
    }
    else if (grade >= 77.0)
    {
